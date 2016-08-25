16c6eaf52e3d61f0ecc947b6076327fc5a18a5e6
Elena <lena@realrussia.co.uk>
2016-08-25 17:47:37 +0300

    GCD

---
+++ 2_3 GCD/EuclidGCD/EuclidGCD/Source.cpp
@@ -0,0 +1,43 @@
+﻿/*
+По данным двум числам 1≤a, b≤2⋅10^9 найдите их наибольший общий делитель.
+
+Sample Input 1: 18 35
+Sample Output 1: 1
+
+Sample Input 2: 14159572 63967072
+Sample Output 2: 4
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int F(int, int);
+
+int main(void) {
+	setlocale(LC_ALL, "ru");
+
+	int a, b;
+	int GCD; //НОД
+
+	cin >> a >> b; //ввести с клавиатуры сначала А, потом В
+
+	GCD = F(a, b);
+
+	cout << GCD; //вывести на экран НОД
+
+	system("pause");
+	return 0;
+}
+
+int F(int x, int y)
+{
+	if (x == 0) { return y; }
+	if (y == 0) { return x; }
+
+	if (x >= y) { return F(x%y, y); }
+	if (y >= x) { return F(x, y%x); }
+}
