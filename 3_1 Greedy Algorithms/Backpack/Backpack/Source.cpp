a92a73e9b493eccc5af8c52b57fa4072dd63162e
Elena <lena@realrussia.co.uk>
2016-09-02 17:52:59 +0300

    Жадные алгоритмы, продолжение

---
+++ 3_1 Greedy Algorithms/Backpack/Backpack/Source.cpp
@@ -0,0 +1,86 @@
+﻿/*
+Первая строка содержит количество предметов 1≤n≤10^3 и вместимость рюкзака 0≤W≤2⋅10^6. Каждая из следующих n строк задаёт стоимость 0≤ci≤2⋅10^6 и объём 0<wi≤2⋅10^6 предмета (n, W, ci, wi — целые числа). Выведите максимальную стоимость частей предметов (от каждого предмета можно отделить любую часть, стоимость и объём при этом пропорционально уменьшатся), помещающихся в данный рюкзак, с точностью не менее трёх знаков после запятой.
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <corecrt_malloc.h>
+#include <locale.h>
+
+using namespace std;
+
+void bubbleSort(float**, int);
+
+int main(void) {
+	setlocale(LC_ALL, "ru");
+
+	int n; //количество предметов = длина массивов
+	int W; //вместимость рюкзака
+	int i, j;
+	float **items; //массив предметов (предмет - его полная стоимость - стоимость его 1 части)
+	float maxCost;
+
+	cout << "Укажите количество предметов (n) и вместимость рюкзака(W) ";
+	cin >> n >> W;
+
+	items = new float*[n];
+
+	for (i = 0; i < n; i++) {
+		items[i] = new float[3];
+
+		for (j = 0; j < 2; j++) {
+			cin >> items[i][j];
+		}
+	}
+	for (i = 0; i < n; i++) {
+		items[i][2] = items[i][0] / items[i][1];
+ 	}
+
+	//сортировка по стоимости на единицу объема от большего к меньшему
+	bubbleSort(items, n);
+
+	maxCost = 0;
+
+	for (i = 0; i < n; i++) {
+		if (items[i][1] <= W) {
+			maxCost += items[i][1] * items[i][2];
+			W -= items[i][1];
+
+			cout << W << endl;
+			cout << fixed << maxCost << endl;
+		}
+		else {
+			maxCost += W * items[i][2];
+			W = 0;
+		}
+	}
+
+	cout << fixed << maxCost << endl;
+
+	system("pause");
+	return 0;
+}
+
+
+void bubbleSort(float **array, int size) {
+	float tmp_0, tmp_1, tmp_2; //временные переменные
+	int k, m; //счетчики проходов по массиву
+
+	for (k = 0; k < (size - 1); k++) { //k - номер прохода
+		for (m = 0; m < (size - 1); m++) { //внутренний цикл прохода
+			if (array[m + 1][2] > array[m][2]) {
+				tmp_2 = array[m + 1][2];
+				tmp_1 = array[m + 1][1];
+				tmp_0 = array[m + 1][0];
+
+				array[m + 1][2] = array[m][2];
+				array[m + 1][1] = array[m][1];
+				array[m + 1][0] = array[m][0];
+
+				array[m][2] = tmp_2;
+				array[m][1] = tmp_1;
+				array[m][0] = tmp_0;
+			}
+		}
+	}
+}
---
+++ 3_1 Greedy Algorithms/Components/Components/Source.cpp
@@ -0,0 +1,63 @@
+﻿/*
+Различные слагаемые
+
+По данному числу 1≤n≤10^9 найдите максимальное число k, для которого n можно представить как сумму k различных натуральных слагаемых. 
+Выведите в первой строке число k, во второй — k слагаемых.
+
+Sample Input 1:
+4
+Sample Output 1:
+2
+1 3
+
+Sample Input 2:
+6
+Sample Output 2:
+3
+1 2 3
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <corecrt_malloc.h>
+#include <locale.h>
+#include <math.h>
+
+using namespace std;
+
+int main(void) {
+	setlocale(LC_ALL, "ru");
+
+	int n, sum; //число
+	long k; //количество слагаемых
+	int i; //счетчик
+	int *components; //массив слогаемых
+
+	cout << "Введите число: ";
+	cin >> n;
+
+	k = (sqrt(1 + 8 * n) - 1) / 2;
+
+	cout << "k = " << k << endl;
+
+	components = new int[k];
+
+	components[0] = 1;
+	components[k - 1] = n - k * (k - 1) / 2;
+	sum = components[0] + components[k];
+
+	if (k > 2) {
+		for (i = 1; i < (k - 1); i++) {
+			components[i] = components[i - 1] + 1;
+		}
+	}
+	
+	for (i = 0; i < k; i++) {
+		cout << components[i] << " ";
+	}
+
+	delete[]components;
+
+	system("pause");
+	return 0;
+}
